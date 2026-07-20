0a58e140f90bc5676c34fcf07e9b068dc4961332
ankwentamo-boop <ankwentamo@gmail.com>
2026-07-20 23:13:18 +0100

    edited the nav and other edits

---
+++ script.js
@@ -0,0 +1,7 @@
+const sideBarEl = document.querySelector(".sidebar");
+function showSidebar() {
+    sideBarEl.style.display = "flex";
+}
+function hideSidebar() {
+    sideBarEl.style.display = "none";
+}
